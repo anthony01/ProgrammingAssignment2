c0a7edf5b2f78a4cb68cfed8e447ce0a073bfb5b
Antony Yatcheny <antony.yatcheny@gmail.com>
2014-12-21 22:23:09 +0300

    v1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Constructs 3 basic methods: get, setinv, getinv for handling cached (square) matrix
 
-## Write a short comment describing this function
+makeCacheMatrix <- function (x = matrix()) {
+        
+        n   <- nrow(x)
+        inv <- matrix(NA, n, n)                                 # Initialize inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        get    <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        
+        list(get = get,
+             setinv = setinv,
+             getinv = getinv)        
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...) {                               # Returns a matrix that is an inverse of 'x'
+        
+        x.inv <- x$getinv()
+        if(all(is.na(x.inv))) {                                 # All NAs mean that inverse has not been evaluated yet
+                message("Calculating Inverse...")
+                x.inv <- x$get()
+                x.inv <- solve(x.inv)                           # If matrix appears to be noninvertable 'solve' returns an error
+                x$setinv(x.inv)
+        }
+        return(x.inv)
 }
+
+# Sample code for testing: (can be easily uncommented by <shift><ctrl>C in RStudio)
+a  <- matrix(runif(16), 4, 4) + diag(nrow = 4) * 3              # Make sample invertable matrix
+x  <- makeCacheMatrix(a)
+b1 <- cacheSolve(x)                                             # First run (calculate inverse using 'solve()')
+b2 <- cacheSolve(x)                                             # Second run (get inverse from cache)
+cat("Check: The message 'Calculating Inverse...' appears ONCE illustrating that the second time an inverse was taken from cache\n\n")
+cat("Check: a %*% b1  should give identity matrix ensuring that 'b1' is an inverse of 'a'\n")
+print(a %*% b1)
+cat("\n")
+cat("Check: b1 - b2 should give all zero matrix, ensuring that cache also did provide an (inverse) matrix\n")
+print(b1 - b2)
